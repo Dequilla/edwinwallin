f72b2cd34feb216041277dfca5eacd1fe6fca9cd
Edwin Wallin <edwin.wallin@dequilla.com>
2024-07-10 19:22:25 +0200

    Added config loading via serde and serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::fs;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+extern crate serde;
+extern crate serde_json;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    port: u16,
+    address: String
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -17,13 +28,17 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let config_file = fs::read_to_string("/etc/edwinwallin/config.json").expect("Could not find config file in \"/etc/edwinwallin/config.json\"");
+    let config: Config = serde_json::from_str(&config_file).expect("Config could not load. Failed to parse json to Config struct.");
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((config.address, config.port))?
     .run()
     .await
 }
